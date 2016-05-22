a453e9e16bd6c9b27a7f5cee809bd6b6db703417
Yasser Carreón <yacasia@gmail.com>
2016-05-22 17:13:18 -0500

    Add ajax to get the client data
    The ajax set the cond and form of payment and the vendor from client too in a new order form.

--- htdocs/commande/card.php
+++ htdocs/commande/card.php
@@ -2459,5 +2459,35 @@ if ($action == 'create' && $user->rights->commande->creer)
 	}
 }
 
+?>
+
+<script type="text/javascript" language="javascript">
+
+	$("#socid").change(function()
+	{
+		var socid = document.getElementById("socid").value;
+	    var client = $("#socid option:selected").text();
+
+	    var params = {  "socid" : socid };
+
+	    $.ajax(
+	    {
+	        data: params,
+	        url: "../../scripts/commande/getClientData.php",
+	        type: "post",
+	        dataType: "json",
+	        success:  function (data) 
+	        {
+	            document.getElementsByName("cond_reglement_id")[0].value = data.cond;
+	            document.getElementById("selectmode_reglement_id").value = data.mode;
+	            document.getElementById("options_vendor").value = data.vendor;
+	        }
+	    });
+	});
+
+</script>
+
+<?php
+
 llxFooter();
 $db->close();
