4f946c5a648101ae07fb2986dc80bad4c5f929eb
Yasser Carreón <yacasia@gmail.com>
2016-06-29 19:05:42 -0500

    Recover script folder
    Some ajax scripts were in that folder.

---
+++ scripts/commande/getClientData.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once("../../htdocs/master.inc.php");
+
+$db->begin();
+$error = 0;
+
+$socid = $_POST['socid'];
+
+$sql = " SELECT s.rowid, s.mode_reglement, s.cond_reglement, se.vendor ";
+$sql.= " FROM ".MAIN_DB_PREFIX."societe s ";
+$sql.= " LEFT JOIN ".MAIN_DB_PREFIX."societe_extrafields se ON se.fk_object = s.rowid";
+$sql.= " WHERE s.rowid = '".$socid."' LIMIT 1";
+
+$resql=$db->query($sql);
+if ($resql)
+{
+	$num = $db->num_rows($resql);
+	$i = 0;
+	if ($num)
+	{
+		while ($i < $num)
+		{
+			$obj = $db->fetch_object($resql);
+			if ($obj)
+			{
+				if ( $obj->mode_reglement) {
+					$resultado["mode"]= $obj->mode_reglement;
+				}else{
+					$resultado["mode"]= "";
+				}
+				if ($obj->cond_reglement) {
+					$resultado["cond"]= $obj->cond_reglement;
+				}else{
+					$resultado["cond"]= "";
+				}
+				if ($obj->vendor) {
+					$resultado["vendor"]= $obj->vendor;
+				}else{
+					$resultado["vendor"]= "";
+				}
+				echo json_encode($resultado);
+			}
+			$i++;
+		}
+	}
+}
+else
+{
+	$error++;
+	dol_print_error($db);
+}
+if (! $error)
+{
+	$db->commit();
+}
+else
+{
+	$db->rollback();
+}
+
+$db->close();
+
+?>
