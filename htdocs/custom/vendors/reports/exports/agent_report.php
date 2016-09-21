d40ddd3e6f0485fb155ffd7c2d8b93a6fb9e6b53
Yasser Carreón <yacasia@gmail.com>
2016-09-20 20:49:48 -0500

    Implement the same changes of agent report in system
    - The "VENTAS" column now represents the amount of invoices made between a date range.
    - The "COBRADO" column now represents the total amount of payments made between a date range.
    - The "SALDO" column now represents the balance from the very begining.*
    - The "VENCIDO" columns now represents the balance that is out of the limit date. *
    
    * These don't consider the balance until the last day of date range, return a global result.

--- htdocs/custom/vendors/reports/exports/agent_report.php
+++ htdocs/custom/vendors/reports/exports/agent_report.php
@@ -16,7 +16,11 @@
     $reyear     = GETPOST('reyear','alpha');
     $remonth    = GETPOST('remonth','alpha');
     $fromDate   = GETPOST('fromDate','alpha');
+        $fromDate = strtotime($fromDate);
+        $fromDate = date("Y-m-d", $fromDate);
     $toDate     = GETPOST('toDate','alpha');
+        $toDate = strtotime($toDate);
+        $toDate = date("Y-m-d", $toDate);
 
     require_once '../../Class/PHPExcel.php';
     $objPHPExcel = new PHPExcel();
@@ -50,45 +54,93 @@
         
         $total_sales = 0;
         $total_amount = 0;
-        $total_debit = 0;
+        $total_balance = 0;
+        $total_due_balance = 0;
 
         while ($vendor = $db->fetch_object($resql_vendors)) {
 
             $sales = 0;
             $amount = 0;
-            $debit = 0;
+            $balance = 0;
+            $due_balance = 0;
+
+            // Colum 1
 
             $sql_fac =  " SELECT * FROM ".MAIN_DB_PREFIX."facture f";
             $sql_fac .= " JOIN ".MAIN_DB_PREFIX."facture_extrafields fe ON fe.fk_object = f.rowid ";
             $sql_fac .= " WHERE fe.vendor = ".$vendor->rowid;
+            if ($fromDate && $toDate) {
+                $sql_fac.= " AND f.datef BETWEEN '".$fromDate."' AND '".$toDate."'";
+            }
 
             $resql_fac = $db->query($sql_fac);
-            if($sql_fac) {
+            if($resql_fac) {
                 while($invoice = $db->fetch_object($resql_fac)) {
-
-                    $sales += $invoice->total_ttc;
-
-                    $sql =  " SELECT * FROM ".MAIN_DB_PREFIX."paiement p ";
-                    $sql .= " JOIN ".MAIN_DB_PREFIX."paiement_facture pf ON pf.fk_paiement = p.rowid ";
-                    $sql .= " WHERE pf.fk_facture = ".$invoice->rowid;
-                    if ($fromDate && $toDate) {
-                        $sql.= " AND p.datep BETWEEN '".$fromDate."' AND '".$toDate."'";
-                    }
-
-                    $resql = $db->query($sql);
-                    if($resql) {                                    
-                        while($payment = $db->fetch_object($sql)) {
-                            $amount += $payment->amount;
-                        }
-                    }
+                    $sales += $invoice->total_ttc;              
                 }
-            }   
+            }
 
-            $debit = $sales - $amount;
+            // Column 2
+
+            $sql_pay  = " SELECT * FROM ".MAIN_DB_PREFIX."paiement p ";
+            $sql_pay .= " JOIN ".MAIN_DB_PREFIX."paiement_facture pf ON pf.fk_paiement = p.rowid ";
+            $sql_pay .= " JOIN ".MAIN_DB_PREFIX."facture_extrafields fe ON fe.fk_object = pf.fk_facture ";
+            $sql_pay .= " WHERE fe.vendor = ".$vendor->rowid;
+            if ($fromDate && $toDate) {
+                $sql_pay.= " AND p.datep BETWEEN '".$fromDate."' AND '".$toDate."'";
+            }
+
+            $resql_pay = $db->query($sql_pay);
+            if($resql_pay) {                                    
+                while($payment = $db->fetch_object($resql_pay)) {
+                    $amount += $payment->amount;
+                }
+            }               
+
+            // Column 3
+
+            $sql_fac =  " SELECT DISTINCT f.fk_soc FROM ".MAIN_DB_PREFIX."facture f";
+            $sql_fac .= " JOIN ".MAIN_DB_PREFIX."facture_extrafields fe ON fe.fk_object = f.rowid ";
+            $sql_fac .= " WHERE fe.vendor = ".$vendor->rowid;
+            if ($fromDate && $toDate) {
+                $sql_fac.= " AND f.datef <= '".$toDate."'";
+            }     
+
+            $resql_fac = $db->query($sql_fac);
+            if($resql_fac) {
+                while($invoice = $db->fetch_object($resql_fac)) {
+                    $soc = new Societe($db);
+                    if ($invoice->fk_soc > 0)
+                        $res = $soc->fetch($invoice->fk_soc);
+                    if($res)
+                        $balance += $soc->get_OutstandingBill();            
+                }
+            }
+
+            // Column 4
+
+            $sql_fac =  " SELECT DISTINCT f.fk_soc FROM ".MAIN_DB_PREFIX."facture f";
+            $sql_fac .= " JOIN ".MAIN_DB_PREFIX."facture_extrafields fe ON fe.fk_object = f.rowid ";
+            $sql_fac .= " WHERE fe.vendor = ".$vendor->rowid;
+            if ($fromDate && $toDate) {
+                $sql_fac.= " AND f.date_lim_reglement < '".$toDate."'";
+            }
+
+            $resql_fac = $db->query($sql_fac);
+            if($resql_fac) {
+                while($invoice = $db->fetch_object($resql_fac)) {
+                    $soc = new Societe($db);
+                    if ($invoice->fk_soc > 0)
+                        $res = $soc->fetch($invoice->fk_soc);
+                    if($res)
+                        $due_balance += $soc->get_OutstandingBill();            
+                }
+            }
 
             $total_sales += $sales;
             $total_amount += $amount;
-            $total_debit += $debit;
+            $total_balance += $balance;
+            $total_due_balance += $due_balance;
 
             $user = new User($db);
             $user->fetch($vendor->rowid);
@@ -99,8 +151,8 @@
             $objPHPExcel->setActiveSheetIndex(0)->getStyle('C'.$row_index.':F'.$row_index)->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER); 
             $objPHPExcel->setActiveSheetIndex(0)->setCellValue('C'.$row_index, $sales);
             $objPHPExcel->setActiveSheetIndex(0)->setCellValue('D'.$row_index, $amount);
-            $objPHPExcel->setActiveSheetIndex(0)->setCellValue('E'.$row_index, $debit);
-            $objPHPExcel->setActiveSheetIndex(0)->setCellValue('F'.$row_index, $unknown);                
+            $objPHPExcel->setActiveSheetIndex(0)->setCellValue('E'.$row_index, $balance);
+            $objPHPExcel->setActiveSheetIndex(0)->setCellValue('F'.$row_index, $due_balance);                
 
             $row_index++;
 
@@ -111,8 +163,8 @@
         $objPHPExcel->setActiveSheetIndex(0)->setCellValue('B'.$row_index, 'TOTALES');  
         $objPHPExcel->setActiveSheetIndex(0)->setCellValue('C'.$row_index, $total_sales);
         $objPHPExcel->setActiveSheetIndex(0)->setCellValue('D'.$row_index, $total_amount);
-        $objPHPExcel->setActiveSheetIndex(0)->setCellValue('E'.$row_index, $total_debit);
-        $objPHPExcel->setActiveSheetIndex(0)->setCellValue('F'.$row_index, $unknown);    
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue('E'.$row_index, $total_balance);
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue('F'.$row_index, $total_due_balance);    
 
     }
     
