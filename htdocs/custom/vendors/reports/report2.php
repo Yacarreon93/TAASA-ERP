7a7ca0428cbd4d9afd5769e7c3446a306ea764d8
Yasser Carreón <yacasia@gmail.com>
2016-09-28 18:48:41 -0500

    A lot of changes in zone report
    - The "VENTAS" column now represents the amount of invoices made between a date range.
    - The "COBRADO" column now represents the total amount of payments made between a date range.
    - The "SALDO" column now represents the balance from the very begining.
    - The "VENCIDO" columns now represents the balance that is out of the limit date.

--- htdocs/custom/vendors/reports/report2.php
+++ htdocs/custom/vendors/reports/report2.php
@@ -17,6 +17,18 @@ $langs->load("users");
 $langs->load("companies");
 
 $action = GETPOST('action', 'alpha');
+$syear = GETPOST("reyear")?GETPOST("reyear"):date("Y", time());
+$cmonth = GETPOST("remonth")?GETPOST("remonth"):date("n", time());
+$fromDate = GETPOST("fromDate")?GETPOST("fromDate"):'';
+if($fromDate) {
+	$fromDate = strtotime($fromDate);
+	$fromDate = date("Y-m-d", $fromDate);
+}
+$toDate = GETPOST("toDate")?GETPOST("toDate"):'';
+if($toDate) {
+	$toDate = strtotime($toDate);
+	$toDate = date("Y-m-d", $toDate);
+}
 
 /*
  * View
@@ -33,12 +45,6 @@ print_fiche_titre($titre,'','title_accountancy.png');
 print '<form method="get" action="">';
 print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
 print '<input type="hidden" name="action" value="report">';
-
-$syear = GETPOST("reyear")?GETPOST("reyear"):date("Y", time());
-$cmonth = GETPOST("remonth")?GETPOST("remonth"):date("n", time());
-$fromDate = GETPOST("fromDate")?GETPOST("fromDate"):'';
-$toDate = GETPOST("toDate")?GETPOST("toDate"):'';
-
 print $formother->select_year($syear,'reyear');
 print $formother->select_month($cmonth,'remonth');
 print ' <select id="reperiod" name="reperiod"></select>';
@@ -107,6 +113,8 @@ if($action == 'report') {
 
 	if($fromDate && $toDate) {
 
+		echo "<div class='titre' style='margin-bottom:10px'>Periodo: del ".$fromDate." al ".$toDate."</div>";
+
 	    print '<form method="GET" action="'.$_SERVER["PHP_SELF"].'">';
 	    print '<table class="noborder" width="100%">';
 	    print '<tr class="liste_titre">';
@@ -116,7 +124,9 @@ if($action == 'report') {
 	    print_liste_field_titre("Saldo",$_SERVER["PHP_SELF"],"s.nom","",$paramlist,"",$sortfield,$sortorder);
 	    print_liste_field_titre("Vencido",$_SERVER["PHP_SELF"],"s.nom","",$paramlist,"",$sortfield,$sortorder);
 
-		$sql_zones =  " SELECT * FROM ".MAIN_DB_PREFIX."c_zones z WHERE z.rowid IN (SELECT DISTINCT se.fk_zone FROM ".MAIN_DB_PREFIX."societe_extrafields se)";
+		$sql_zones =  " SELECT * FROM ".MAIN_DB_PREFIX."c_zones z ";
+		$sql_zones .= " WHERE z.rowid IN ";
+		$sql_zones .= " (SELECT DISTINCT se.fk_zone FROM ".MAIN_DB_PREFIX."societe_extrafields se)";
 
 		$resql_zones = $db->query($sql_zones);
 		if($resql_zones) {
@@ -125,62 +135,119 @@ if($action == 'report') {
 
 				$sales = 0;
 				$amount = 0;
-				$debit = 0;
+				$balance = 0;
+				$due_balance = 0;
+
+				// Column 1
 
 				$sql_fac =  " SELECT * FROM ".MAIN_DB_PREFIX."facture f";
 				$sql_fac .= " JOIN ".MAIN_DB_PREFIX."societe s ON s.rowid = f.fk_soc ";
 				$sql_fac .= " JOIN ".MAIN_DB_PREFIX."societe_extrafields se ON se.fk_object = s.rowid ";
-				$sql_fac .= " WHERE se.fk_zone = ".$zone->rowid;
+				$sql_fac .= " WHERE ";
+				$sql_fac .= " f.fk_statut <> 0";		// Not a draft
+				$sql_fac .= " AND f.fk_statut <> 3";	// Not abandonned
+				$sql_fac .= " AND se.fk_zone = ".$zone->rowid;
+				$sql_fac .= " AND f.datef BETWEEN '".$fromDate."' AND '".$toDate."'";
 
 				$resql_fac = $db->query($sql_fac);
-
-				echo $sql_fac;
-
 				if($resql_fac) {
+					while($invoice = $db->fetch_object($resql_fac)) {
+						$sales += $invoice->total_ttc;				
+					}
+				}
 
-					if($resql_fac->num_rows <= 0) {
-						continue;
-					} 
+				// Column 2
+
+				$sql_pay  = " SELECT * FROM ".MAIN_DB_PREFIX."paiement p ";
+				$sql_pay .= " JOIN ".MAIN_DB_PREFIX."paiement_facture pf ON pf.fk_paiement = p.rowid ";
+				$sql_pay .= " JOIN ".MAIN_DB_PREFIX."facture f ON f.rowid = pf.fk_facture ";
+				$sql_pay .= " JOIN ".MAIN_DB_PREFIX."societe s ON s.rowid = f.fk_soc ";
+				$sql_pay .= " JOIN ".MAIN_DB_PREFIX."societe_extrafields se ON se.fk_object = s.rowid ";
+				$sql_pay .= " WHERE ";
+				$sql_pay .= " f.fk_statut <> 0";		// Not a draft
+				$sql_pay .= " AND f.fk_statut <> 3";	// Not abandonned
+				$sql_pay .= " AND se.fk_zone = ".$zone->rowid;
+				$sql_pay .= " AND p.datep BETWEEN '".$fromDate."' AND '".$toDate."'";
+
+	            $resql_pay = $db->query($sql_pay);
+				if($resql_pay) {									
+					while($payment = $db->fetch_object($resql_pay)) {
+						$amount += $payment->amount;
+					}
+				}
+
+				// Column 3
+
+				$sql_fac =  " SELECT DISTINCT f.fk_soc FROM ".MAIN_DB_PREFIX."facture f";
+				$sql_fac .= " JOIN ".MAIN_DB_PREFIX."societe s ON s.rowid = f.fk_soc ";
+				$sql_fac .= " JOIN ".MAIN_DB_PREFIX."societe_extrafields se ON se.fk_object = s.rowid ";
+				$sql_fac .= " WHERE ";
+				$sql_fac .= " f.fk_statut <> 0";		// Not a draft
+				$sql_fac .= " AND f.fk_statut <> 3";	// Not abandonned
+				$sql_fac .= " AND se.fk_zone = ".$zone->rowid;
+	            $sql_fac .= " AND f.datef <= '".$toDate."'";
+
+				$resql_fac = $db->query($sql_fac);
+				if($resql_fac) {
+					while($invoice = $db->fetch_object($resql_fac)) {
+						$soc = new Societe($db);
+						if ($invoice->fk_soc > 0)
+							$res = $soc->fetch($invoice->fk_soc);
+						if($res) {									
+							$balance += $soc->get_OutstandingBill($toDate);			
+						}
+					}
+				}
+
+				// Column 4
+
+				$sql_fac  = " SELECT f.rowid, f.total_ttc FROM ".MAIN_DB_PREFIX."facture as f";
+				$sql_fac .= " JOIN ".MAIN_DB_PREFIX."societe s ON s.rowid = f.fk_soc ";
+				$sql_fac .= " JOIN ".MAIN_DB_PREFIX."societe_extrafields se ON se.fk_object = s.rowid ";
+				$sql_fac .= " WHERE ";
+				$sql_fac .= " f.paye = 0";
+				$sql_fac .= " AND f.fk_statut <> 0";	// Not a draft
+				$sql_fac .= " AND f.fk_statut <> 3";	// Not abandonned
+				$sql_fac .= " AND f.fk_statut <> 2";
+				$sql_fac .= " AND se.fk_zone = ".$zone->rowid;
+	            $sql_fac .= " AND f.date_lim_reglement < '".$toDate."'";		
+
+				$resql_fac = $db->query($sql_fac);
+				if($resql_fac) {
+					
+					require_once DOL_DOCUMENT_ROOT.'/compta/facture/class/facture.class.php';
+					$facturestatic = new Facture($db);
 
 					while($invoice = $db->fetch_object($resql_fac)) {
 
-						$sales += $invoice->total_ttc;
+						$facturestatic->id = $invoice->rowid;
+						$paiement = $facturestatic->getSommePaiement($toDate);
 
-						$sql =  " SELECT * FROM ".MAIN_DB_PREFIX."paiement p ";
-						$sql .= " JOIN ".MAIN_DB_PREFIX."paiement_facture pf ON pf.fk_paiement = p.rowid ";
-						$sql .= " WHERE pf.fk_facture = ".$invoice->rowid;
-						if ($fromDate && $toDate) {
-			                $sql.= " AND p.datep BETWEEN '".$fromDate."' AND '".$toDate."'";
-			            }
-
-						$resql = $db->query($sql);
-						if($resql) {									
-							while($payment = $db->fetch_object($sql)) {
-								$amount += $payment->amount;
-							}
-						}
+						$due_balance += $invoice->total_ttc - $paiement;					
 					}
+				}				
+				
+				$user = new User($db);
+		        $user->fetch($vendor->rowid);
 
-					$debit = $sales - $amount;
+				$var=!$var;
+				print "<tr ".$bc[$var].">";
+				print '<td>';	        
+		        print $zone->nom;
+		        print '</td>';
+		        print '<td>';
+		        print $sales;
+		        print '</td>';
+		        print '<td>';
+		        print $amount;
+		        print '</td>';
+		        print '<td>';
+		        print $balance;
+		        print '</td>';
+		        print '<td>';
+		        print $due_balance;
+		        print '</td>';
 
-					$var=!$var;
-					print "<tr ".$bc[$var].">";
-					print '<td>';	        
-			        print $zone->nom;
-			        print '</td>';
-			        print '<td>';
-			        print $sales;
-			        print '</td>';
-			        print '<td>';
-			        print $amount;
-			        print '</td>';
-			        print '<td>';
-			        print $debit;
-			        print '</td>';
-			        print '<td>';
-			        print $unknown;
-			        print '</td>';
-				}									
 			}
 		}
 
