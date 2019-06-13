584e53b361c5d8b2446ccf61444f04514b48b506
Luis Mira <luis.mirar@gmail.com>
2019-06-13 00:36:32 -0500

    Fixed commission report

--- htdocs/custom/vendors/reports/exports/commission_report.php
+++ htdocs/custom/vendors/reports/exports/commission_report.php
@@ -25,55 +25,26 @@ $object = new User($db);
 $res = $object->fetch($id);
 if ($res < 0) { dol_print_error($db,$object->error); exit; }
 
-$sql = 'SELECT';
-if ($sall || $search_product_category > 0) $sql = 'SELECT DISTINCT';
-$sql.= ' f.rowid as facid, f.facnumber, f.ref_client, f.type, f.note_private, f.increment, f.total as total_ht, f.tva as total_tva, f.total_ttc,';
-$sql.= ' f.datef as df, f.date_lim_reglement as datelimite,';
-$sql.= ' f.paye as paye, f.fk_statut,';
-$sql.= ' s.nom as name, s.rowid as socid, s.code_client, s.client, se.commission as com';
-if (! $sall) $sql.= ', SUM(pf.amount) as am';   // To be able to sort on status
-$sql.= ' FROM '.MAIN_DB_PREFIX.'societe as s';
-$sql.= ' JOIN '.MAIN_DB_PREFIX.'societe_extrafields AS se ON se.fk_object = s.rowid';
-$sql.= ', '.MAIN_DB_PREFIX.'facture as f';
-if (! $sall) $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'paiement_facture as pf ON pf.fk_facture = f.rowid';
-else $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'facturedet as fd ON fd.fk_facture = f.rowid';
-if ($sall || $search_product_category > 0) $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'facturedet as pd ON f.rowid=pd.fk_facture';
-if ($search_product_category > 0) $sql.= ' LEFT JOIN '.MAIN_DB_PREFIX.'categorie_product as cp ON cp.fk_product=pd.fk_product';
-// We'll need this table joined to the select in order to filter by sale
-if ($search_sale > 0 || (! $user->rights->societe->client->voir && ! $socid)) $sql .= ", ".MAIN_DB_PREFIX."societe_commerciaux as sc";
-if ($search_user > 0)
-{
-    $sql.=", ".MAIN_DB_PREFIX."element_contact as ec";
-    $sql.=", ".MAIN_DB_PREFIX."c_type_contact as tc";
-}
-$sql.= ' JOIN '.MAIN_DB_PREFIX.'facture_extrafields as ef ON ef.fk_object = f.rowid';
-$sql.= ' WHERE f.fk_soc = s.rowid';
-$sql.= ' AND ef.vendor = '.$id;
-$sql.= ' AND f.fk_statut = 2';
-$sql.= " AND f.entity = ".$conf->entity;
-if (! $user->rights->societe->client->voir && ! $socid) $sql.= " AND s.rowid = sc.fk_soc AND sc.fk_user = " .$user->id;
-if ($search_product_category > 0) $sql.=" AND cp.fk_categorie = ".$search_product_category;
-if ($socid > 0) $sql.= ' AND s.rowid = '.$socid;
-if ($userid)
-{
-    if ($userid == -1) $sql.=' AND f.fk_user_author IS NULL';
-    else $sql.=' AND f.fk_user_author = '.$userid;
-}
-if ($filtre)
-{
-    $aFilter = explode(',', $filtre);
-    foreach ($aFilter as $filter)
-    {
-        $filt = explode(':', $filter);
-        $sql .= ' AND ' . trim($filt[0]) . ' = ' . trim($filt[1]);
-    }
-}
-if ($search_ref) $sql .= natural_search('f.facnumber', $search_ref);
-if ($search_refcustomer) $sql .= natural_search('f.ref_client', $search_refcustomer);
-if ($search_societe) $sql .= natural_search('s.nom', $search_societe);
-if ($search_montant_ht != '') $sql.= natural_search('f.total', $search_montant_ht, 1);
-if ($search_montant_ttc != '') $sql.= natural_search('f.total_ttc', $search_montant_ttc, 1);
-if ($search_status != '' && $search_status >= 0) $sql.= " AND f.fk_statut = ".$db->escape($search_status);
+$sql = 'SELECT DISTINCT
+	p.rowid,
+	f.facnumber,
+	s.nom AS name,
+	datef as df,
+	total_ttc,
+	p.datep,
+	f.date_lim_reglement,
+	pf.amount,
+	se.commission
+FROM
+	llx_facture AS f
+RIGHT JOIN llx_paiement_facture AS pf ON pf.fk_facture = f.rowid
+JOIN llx_paiement AS p ON pf.fk_paiement = p.rowid
+JOIN llx_societe AS s ON f.fk_soc = s.rowid
+JOIN llx_societe_extrafields AS se ON se.fk_object = s.rowid
+JOIN llx_facture_extrafields AS fe ON f.rowid = fe.fk_object
+WHERE
+	f.fk_statut != 3 AND fe.vendor = '.$id.' AND (p.datep < f.date_lim_reglement OR DATE(p.datep) = DATE(f.date_lim_reglement))';
+
 if ($fromDate && $toDate) {
     $sql.= " AND f.datef BETWEEN '".$fromDate."' AND '".$toDate."'";
 }
@@ -103,23 +74,8 @@ else if ($year_lim > 0)
 {
     $sql.= " AND f.date_lim_reglement BETWEEN '".$db->idate(dol_get_first_day($year_lim,1,false))."' AND '".$db->idate(dol_get_last_day($year_lim,12,false))."'";
 }
-if ($search_sale > 0) $sql.= " AND s.rowid = sc.fk_soc AND sc.fk_user = " .$search_sale;
-if ($search_user > 0)
-{
-    $sql.= " AND ec.fk_c_type_contact = tc.rowid AND tc.element='facture' AND tc.source='internal' AND ec.element_id = f.rowid AND ec.fk_socpeople = ".$search_user;
-}
-if (! $sall)
-{
-    $sql.= ' GROUP BY f.rowid, f.facnumber, ref_client, f.type, f.note_private, f.increment, f.total, f.tva, f.total_ttc,';
-    $sql.= ' f.datef, f.date_lim_reglement,';
-    $sql.= ' f.paye, f.fk_statut,';
-    $sql.= ' s.nom, s.rowid, s.code_client, s.client';
-}
-else
-{
-    $sql .= natural_search(array('s.nom', 'f.facnumber', 'f.note_public', 'fd.description'), $sall);
-}
 $sql.= $db->order($sortfield,$sortorder);
+
 
 $result = $db->query($sql);
 $num = $db->num_rows($result);
@@ -133,12 +89,12 @@ $pdf->AddPage();
 
 $pdf->SetFont('Arial', 'B', 8);
 
-if($object) 
+if($object)
 {
     // Print the name of vendor
     $pdf->Cell(80, 10, strtoupper($object->firstname.' '.$object->lastname), 0, 0, 'L');
-} 
-else 
+}
+else
 {
     $pdf->Cell(80, 10, '', 0, 0, 'L');
 }
@@ -170,19 +126,19 @@ $pdf->SetFillColor(232,232,232);
 $pdf->SetFont('Arial','B',6);
 // $pdf->SetY($Y_Fields_Name_position);
 // $pdf->SetX(20);
-$pdf->Cell(30,6,'FACTURA',1,0,'L',1);
+$pdf->Cell(30,6,'PAGO',1,0,'L',1);
 $pdf->SetX(40);
-$pdf->Cell(20,6,'FECHA',1,0,'L',1);
+$pdf->Cell(20,6,'FACTURA',1,0,'L',1);
 $pdf->SetX(60);
-$pdf->Cell(20,6,'VENCIMIENTO',1,0,'L',1);
-$pdf->SetX(80);
 $pdf->Cell(20,6,'CLIENTE',1,0,'L',1);
+$pdf->SetX(80);
+$pdf->Cell(20,6,'VENCIMIENTO',1,0,'L',1);
 $pdf->SetX(100);
-$pdf->Cell(20,6,'IMPORTE BASE',1,0,'R',1);
+$pdf->Cell(20,6,'TOTAL',1,0,'R',1);
 $pdf->SetX(120);
-$pdf->Cell(20,6,'IMPORTE IVA',1,0,'R',1);
+$pdf->Cell(20,6,'FECHA',1,0,'R',1);
 $pdf->SetX(140);
-$pdf->Cell(20,6,'IMPORTE TOTAL',1,0,'R',1);
+$pdf->Cell(20,6,'ABONO',1,0,'R',1);
 $pdf->SetX(160);
 $pdf->Cell(20,6,'% COMISION',1,0,'C',1);
 $pdf->SetX(180);
@@ -207,35 +163,35 @@ while($row = $db->fetch_object($result))
 
     // $pdf->SetY($Y_Fields_Name_position);
     // $pdf->SetX(20);
-    $pdf->Cell(30,6,$row->facnumber,1,0,'L',1);
+    $pdf->Cell(30,6,$row->rowid,1,0,'L',1);
     $pdf->SetX(40);
-    $pdf->Cell(20,6,dol_print_date($db->jdate($row->df),'day'),1,0,'L',1);
+    $pdf->Cell(20,6,$row->facnumber,1,0,'L',1);
     $pdf->SetX(60);
-    $pdf->Cell(20,6,dol_print_date($db->jdate($row->datelimite),'day'),1,0,'L',1);
-    $pdf->SetX(80);
     $pdf->Cell(20,6,$row->name,1,0,'L',1);
+    $pdf->SetX(80);
+    $pdf->Cell(20,6,dol_print_date($db->jdate($row->df),'day'),1,0,'L',1);
     $pdf->SetX(100);
-    $pdf->Cell(20,6,price($row->total_ht,0,$langs),1,0,'R',1);
-    $pdf->SetX(120);
-    $pdf->Cell(20,6,price($row->total_tva,0,$langs),1,0,'R',1);
-    $pdf->SetX(140);
     $pdf->Cell(20,6,price($row->total_ttc,0,$langs),1,0,'R',1);
+    $pdf->SetX(120);
+    $pdf->Cell(20,6,dol_print_date($db->jdate($row->datep),'day'),1,0,'L',1);
+    $pdf->SetX(140);
+    $pdf->Cell(20,6,price($row->amount,0,$langs),1,0,'R',1);
 
-    $special_commission = $row->com;
+    $special_commission = $row->commission;
     if($special_commission > 0) {
-        $total_commission += ($row->total_ttc * ($special_commission)/ 100);
+        $total_commission += ($row->amount * ($special_commission)/ 100);
         $pdf->SetX(160);
         $pdf->Cell(20,6,number_format($special_commission,2),1,0,'C',1);
         $pdf->SetX(180);
-        $pdf->Cell(20,6,($row->total_ttc * ($special_commission)/ 100),1,0,'R',1);
+        $pdf->Cell(20,6,price($row->amount * ($special_commission)/ 100),1,0,'R',1);
     }
     else {
-        $total_commission += ($row->total_ttc * ($object->array_options['options_commission'])/ 100);
+        $total_commission += ($row->amount * ($object->array_options['options_commission'])/ 100);
         $pdf->SetX(160);
         $pdf->Cell(20,6,number_format($object->array_options['options_commission'],2),1,0,'C',1);
         $pdf->SetX(180);
-        $pdf->Cell(20,6,($row->total_ttc * ($object->array_options['options_commission'])/ 100),1,0,'R',1);
-    }    
+        $pdf->Cell(20,6,price($row->amount * ($object->array_options['options_commission'])/ 100),1,0,'R',1);
+    }
 
     // $code = $row["ref"];
     // $name = substr($row["label"],0,20);
